3ebb755a437beb4cce9c87a91490ea05eecb0a78
Nikola <nikolavukic333@gmail.com>
2022-12-31 11:50:51 +0100

    Ponderisao minimax, dodao komentar za minimax

--- main.cpp
+++ main.cpp
@@ -21,42 +21,36 @@ bool isMovePossible(vector<vector<char>> board)
 // Vraca INF/-INF ukoliko je pobjednik X/O, u suprotnom vraca 0
 int evaluateBoard(vector<vector<char>> board, int depth)
 {
-    /* Da bi algoritam uvijek birao potez koji donosi optimalan rezultat u najmanjem
-       broju koraka ponderisacemo dobit sa brojem koraka koji je neophodan da se dodje
-       do konacnog rezultata*/
-    int MAX_VICTORY = INF - depth;
-    int MIN_VICTORY = -INF + depth;
-
     // 3 u redu
     for(int i = 0; i < board.size(); i++) {
         /*cout << "Proveravam da li ima 3 u redu" << endl;
         cout << "Uslov za empty je: " << board[i][0] << endl;*/
         if(board[i][0] != EMPTY && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
-            if(board[i][0] == MAXIMIZER) return MAX_VICTORY;
-            return MIN_VICTORY;
+            if(board[i][0] == MAXIMIZER) return INF;
+            return -INF;
         }
     }
  
     // 3 u koloni
     for(int i = 0; i < board.size(); i++) {
         if(board[0][i] != EMPTY && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
-            if(board[0][i] == MAXIMIZER) return MAX_VICTORY;
-            return MIN_VICTORY;
+            if(board[0][i] == MAXIMIZER) return INF;
+            return -INF;
         }
     }
  
     // Glavna dijagonala
     if (board[1][1] != EMPTY && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))
     {
-        if(board[1][1] == MAXIMIZER) return MAX_VICTORY;
-        return MIN_VICTORY;
+        if(board[1][1] == MAXIMIZER) return INF;
+        return -INF;
     }
  
     // Sporedna dijagonala
     if (board[1][1] != EMPTY && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))
     {
-        if(board[1][1] == MAXIMIZER) return MAX_VICTORY;
-        return MIN_VICTORY;
+        if(board[1][1] == MAXIMIZER) return INF;
+        return -INF;
     }
  
     // Trenutno nema pobjednika
@@ -64,14 +58,19 @@ int evaluateBoard(vector<vector<char>> board, int depth)
 }
  
 /* Radi na principu backtracking odnosno isprobava sve moguće kombinacije
-   i na kraju bira optimalan potez za trenutnog igraca */
+   i na kraju bira optimalan potez za trenutnog igraca. Algoritam je
+   ponderisan dubinom odnosno, ukoliko postoje dva ili vise poteza koji dovode do
+   optimalnog rjesenja, birace se onaj potez koji dovodi do rjesenja u 
+   najmanjem broju koraka. Nacin na koji je algoritam ponderisan je taj što 
+   je broj koraka koji je ostao do pobjede(depth) oduziman od vrijednosti poteza
+   koji ce biti napravljen u slucaju maximizera, odnosno dodavan, za slucaj minimizera. 
+   Tako na primjer ukoliko postoje 2 poteza koja dovode do pobjede do racunara,
+   u ovom slucaju maksimizera, recimo potez A i potez B. Neka A dovodi do pobjede u 2
+   poteza a B u tri poteza. Tada ce A imati vrijednost INF-2 dok će B imati vrijednost
+   INF-3 i maksimum ce uvijek biti potez 2. */
 int minimax(vector<vector<char>> board, int depth, bool isMaximizersMove)
 {
     int currentScore = evaluateBoard(board, depth);
-
-    // Posto je score u evaluateBaord ponderisan on ce se za return uslov morai normalizovati
-    if(isMaximizersMove) currentScore += depth;
-    else currentScore -= depth;
 
     if(currentScore == INF || currentScore == -INF) return currentScore;
     if (!isMovePossible(board)) return 0;
@@ -83,11 +82,13 @@ int minimax(vector<vector<char>> board, int depth, bool isMaximizersMove)
             if(cell == EMPTY) {
                 if(isMaximizersMove) {
                     cell = MAXIMIZER;
-                    bestMoveValue = max(bestMoveValue, minimax(board, depth + 1, !isMaximizersMove));
+                    bestMoveValue = max(bestMoveValue, (minimax(board, depth + 1, !isMaximizersMove) - depth));
+                    // backtracking
                     cell = EMPTY;
                 } else {
                     cell = MINIMIZER;
-                    bestMoveValue = min(bestMoveValue, minimax(board, depth + 1, !isMaximizersMove));
+                    bestMoveValue = min(bestMoveValue, (minimax(board, depth + 1, !isMaximizersMove) + depth));
+                    // backtracking
                     cell = EMPTY;
                 }
             }
@@ -108,6 +109,7 @@ Move findBestMove(vector<vector<char>> &board)
             if(board[i][j] == EMPTY) {
                 board[i][j] = MAXIMIZER;
                 int currentMoveValue = minimax(board, 0, false);
+                // bactracking
                 board[i][j] = EMPTY;
 
                 if(currentMoveValue > bestMoveValue) {
